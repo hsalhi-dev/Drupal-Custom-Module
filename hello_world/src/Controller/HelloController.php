27b0261c51daa7dd68b00e882be1e29eba493ad7
hsalhi-dev <h.salhi@void.fr>
2026-03-03 12:42:09 +0000

    find and fix errors with PHPCS and PHP Stan

--- hello_world/src/Controller/HelloController.php
+++ hello_world/src/Controller/HelloController.php
@@ -1,14 +1,19 @@
 <?php
+
 /**
  * @file
  * Contains \Drupal\hello_world\Controller\HelloController.
  */
+
 namespace Drupal\hello_world\Controller;
-class HelloController {
-  public function content() {
-    return array(
-      '#type' => 'markup',
-      '#markup' => t('Hello, World!'),
-    );
-  }
+
+class HelloController
+{
+    public function content()
+    {
+        return array(
+        '#type' => 'markup',
+        '#markup' => t('Hello, World!'),
+        );
+    }
 }
